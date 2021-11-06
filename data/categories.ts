4a364c97e0a73a99d09e82c11a95e112d79ce2da
Ian Banda <ian.d.banda@gmail.com>
2021-11-07 01:31:37 +0200

    add id

--- data/categories.ts
+++ data/categories.ts
@@ -1,6 +1,7 @@
 import {ImageSourcePropType} from 'react-native';
 
-interface Category {
+export interface Category {
+  id: number;
   title: string;
   icon: ImageSourcePropType;
   gradientColors: string[];
@@ -8,28 +9,33 @@ interface Category {
 
 export const categories: Category[] = [
   {
+    id: 25,
     title: 'Art',
-    icon: require('../images/paint-kit-front-gradient.png'),
+    icon: require('../images/paint-kit-front-color.png'),
     gradientColors: ['#FF5C58', '#FE8F8F', '#FCD2D1'],
   },
   {
+    id: 10,
     title: 'Books',
-    icon: require('../images/notebook-front-gradient.png'),
+    icon: require('../images/notebook-front-color.png'),
     gradientColors: ['#14279B', '#3D56B2', '#5C7AEA'],
   },
   {
+    id: 26,
     title: 'Celebrities',
-    icon: require('../images/boy-front-gradient.png'),
+    icon: require('../images/boy-front-color.png'),
     gradientColors: ['#3E065F', '#700B97', '#8E05C2'],
   },
   {
+    id: 11,
     title: 'Film',
-    icon: require('../images/video-camera-front-gradient.png'),
+    icon: require('../images/video-camera-front-color.png'),
     gradientColors: ['#38A3A5', '#57CC99', '#80ED99'],
   },
   {
+    id: 12,
     title: 'Music',
-    icon: require('../images/music-front-gradient.png'),
+    icon: require('../images/music-front-color.png'),
     gradientColors: ['#C3BA85', '#DAD5AB', '#F0F0CB'],
   },
 ];
